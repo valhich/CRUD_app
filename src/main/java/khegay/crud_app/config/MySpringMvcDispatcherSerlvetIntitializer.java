0693894f92c0e337defa36dd5c177c80d63a7898
valhich <valhi4@yandex.ru>
2023-03-31 12:09:12 +0300

    Добавлены методы READ и POST

--- src/main/java/khegay/crud_app/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ src/main/java/khegay/crud_app/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -1,2 +1,20 @@
-package khegay.crud_app.config;public class MySpringMVCDispatcherSerlvetIntitializer {
+package khegay.crud_app.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
---
+++ src/main/java/khegay/crud_app/dao/PersonDAO.java
@@ -0,0 +1,35 @@
+package khegay.crud_app.dao;
+
+import khegay.crud_app.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people=new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT,"Tom"));
+        people.add(new Person(++PEOPLE_COUNT,"Jerry"));
+        people.add(new Person(++PEOPLE_COUNT,"Mike"));
+        people.add(new Person(++PEOPLE_COUNT,"Katya"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+    public Person show(int id){
+        return people.stream().filter(person->person.getId()==id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+}
